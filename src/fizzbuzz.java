980dfbb42a97cace3c900488c64270961cb9a85b
vithcode <vitthal.jadhav.9404@gmail.com>
2025-07-26 20:29:30 +0530

    Fizz Buzz

---
+++ src/fizzbuzz.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class fizzbuzz {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt();
+//        if (N%3==0 && N%5==0){
+//            System.out.println("FizzBuzz");
+//        } else if(N%3==0){
+//            System.out.println("Fizz");
+//        }else if(N%5==0){
+//            System.out.println("Buzz");
+//    }}
+//}
+ if(N%3==0){
+        System.out.print("Fizz");
+        } if(N%5==0) {
+            System.out.print("Buzz");
+        }}}
